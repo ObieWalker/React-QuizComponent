52f4d32fdc38b78971698dbdfe75349da85566eb
ObieWalker <obinnawalker@gmail.com>
2018-10-05 20:24:53 +0100

    Completed the module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton'
+import { SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION } from 'constants';
 
 class QuizQuestion extends Component {
-
+handleClick(buttonText) {
+  if(buttonText === this.props.quiz_question.answer) {
+    this.props.showNextQuestionHandler()
+  }
+}
 render() {
   return (
     <main>
@@ -11,7 +16,11 @@ render() {
     </section>
     <section className="buttons">
       <ul>
-        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+        {this.props.quiz_question.answer_options.map((answer_option, index) => {
+          return <QuizQuestionButton key={index} button_text={answer_option}
+          clickHandler={this.handleClick.bind(this)} />
+        })}
+       
       </ul>
     </section>
   </main>
